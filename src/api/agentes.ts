646cfab36c8db2f00fb096aa38b2fa5e1d3842a2
kealabs-ai <kealabs.ai@gmail.com>
2026-06-12 19:27:50 -0300

    fix v1 to k1 on endpoints

--- src/api/agentes.ts
+++ src/api/agentes.ts
@@ -2,10 +2,10 @@ import { api } from './client'
 import type { AgenteIA } from '../types'
 
 export const agentesApi = {
-  list: () => api.get<AgenteIA[]>('/v1/lex/agentes').then((r) => r.data),
-  listPublicos: () => api.get<AgenteIA[]>('/v1/lex/agentes/publicos').then((r) => r.data),
-  get: (id: string) => api.post<AgenteIA>('/v1/lex/agentes/get', { id }).then((r) => r.data),
-  create: (data: Partial<AgenteIA>) => api.post<AgenteIA>('/v1/lex/agentes', data).then((r) => r.data),
-  update: (id: string, data: Partial<AgenteIA>) => api.post<AgenteIA>('/v1/lex/agentes/update', { id, ...data }).then((r) => r.data),
-  delete: (id: string) => api.post('/v1/lex/agentes/delete', { id }),
+  list: () => api.get<AgenteIA[]>('/k1/lex/agentes').then((r) => r.data),
+  listPublicos: () => api.get<AgenteIA[]>('/k1/lex/agentes/publicos').then((r) => r.data),
+  get: (id: string) => api.get<AgenteIA>(`/k1/lex/agentes/${id}`).then((r) => r.data),
+  create: (data: Partial<AgenteIA>) => api.post<AgenteIA>('/k1/lex/agentes', data).then((r) => r.data),
+  update: (id: string, data: Partial<AgenteIA>) => api.put<AgenteIA>(`/k1/lex/agentes/${id}`, data).then((r) => r.data),
+  delete: (id: string) => api.delete(`/k1/lex/agentes/${id}`),
 }
