1d40d1d787f447c5ce8004348d6902c482f7228a
kealabs-ai <kealabs.ai@gmail.com>
2026-07-08 19:29:22 -0300

    fix: update parameter name in avancarFase API call for consistency

--- src/api/processos.ts
+++ src/api/processos.ts
@@ -16,7 +16,7 @@ export const processosApi = {
     api.post<Processo>('/k1/lex/processos/update', { id, ...data }).then((r) => r.data),
   remove: (id: string) => api.post('/k1/lex/processos/delete', { id }).then((r) => r.data),
   avancarFase: (id: string, novaFase: number) => {
-    console.log('avancarFase API call:', { id, novaFase })
-    return api.post<Processo>('/k1/lex/processos/avancar-fase', { id, novaFase }).then((r) => r.data)
+    console.log('avancarFase API call:', { processoId: id, novaFase })
+    return api.post<Processo>('/k1/lex/processos/avancar-fase', { processoId: id, novaFase }).then((r) => r.data)
   },
 }
